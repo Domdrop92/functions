60cb275d42ad08939387319bc8ef3cd9cb77f9b2
Dominique Dixon <domdrop92@hotmail.com>
2019-05-06 20:35:24 -0700

    learned about hoisting

---
+++ js/hoisting.js
@@ -0,0 +1,9 @@
+funcDeclaration ();
+function funcDeclaration(){
+    console.log('I am inside a function declaration.');
+}
+
+var funcExpression = function (){
+    console.log('I am inside a function declaration.');
+};
+
