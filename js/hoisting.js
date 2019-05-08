90bdedb080e015b1b3ca7e7e8896a74b3ac0a7d5
Dominique Dixon <domdrop92@hotmail.com>
2019-05-07 19:07:26 -0700

    worked on hoisting things

--- js/hoisting.js
+++ js/hoisting.js
@@ -1,9 +1,15 @@
+//functions definded by declaration are hoisted to the top
 funcDeclaration ();
 function funcDeclaration(){
     console.log('I am inside a function declaration.');
 }
 
+//functions defined by expression are not hoisted to the top. They remain where they are defined.
+funcExpression();
 var funcExpression = function (){
     console.log('I am inside a function declaration.');
 };
+
+//so to avoid confusion, its best practice is to use functions after they are defined
+
 
